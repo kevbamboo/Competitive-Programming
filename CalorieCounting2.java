68860762e8cf5982653314bc8272bf0bc63fe256
Kevin Song <71475117+kevbamboo@users.noreply.github.com>
2022-12-01 13:10:57 -0600

    Update CalorieCounting2.java

--- CalorieCounting2.java
+++ CalorieCounting2.java
@@ -30,18 +30,18 @@ public class MyClass {
             }
         }
         if (max1 == -1) max1 = elf;
-                if (max2 == -1) max2 = elf;
-                if (max3 == -1) max3 = elf;
-                if (elf >= max1) {
-                    max3 = max2;
-                    max2 = max1;
-                    max1 = elf;
-                } else if (elf >= max2) {
-                    max3 = max2;
-                    max2 = elf;
-                } else if (elf >= max3) {
-                    max3 = elf;
-                }
+        if (max2 == -1) max2 = elf;
+        if (max3 == -1) max3 = elf;
+        if (elf >= max1) {
+            max3 = max2;
+            max2 = max1;
+            max1 = elf;
+        } else if (elf >= max2) {
+            max3 = max2;
+            max2 = elf;
+        } else if (elf >= max3) {
+            max3 = elf;
+        }
         System.out.println(max1+max2+max3);
     }
 }
