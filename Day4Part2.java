03a87fca2075128ee6b54f3ee777274ddb93d801
Kevin Song <71475117+kevbamboo@users.noreply.github.com>
2022-12-04 16:49:37 -0600

    Update and rename Camp2.java to Day4Part2.java

--- Day4Part2.java
+++ Day4Part2.java
@@ -1,6 +1,6 @@
 import java.io.*;
 
-public class Camp2 {
+public class Day4Part2 {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("input.txt"));
         int numContained = 0;
